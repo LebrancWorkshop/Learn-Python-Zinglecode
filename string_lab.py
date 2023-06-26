d5294df42188e510da42d0878bcc6061126a2f46
lebrancconvas <lebrancconvas@gmail.com>
2023-06-26 07:32:42 +0700

    ✨ feat: update lesson: string

--- string_lab.py
+++ string_lab.py
@@ -1,6 +1,41 @@
+# Length of String
+
 message = 'String'
 message_length = len(message)
 print(message_length)
+
+# Check Sub-String contain in String.
+message2 = "Hamon"
+find_message = "mon"
+print(find_message in message2)
+
+# Check Digit
+tel = "0999999999"
+check_digit = tel.isdigit()
+print(check_digit)
+
+# To Upper Case
+message3 = "Just a little quest"
+message3_upper = message3.upper()
+print(message3_upper)
+print(message3)
+
+# Replace String
+quest = "Finding Stroll Star"
+quest_replace = quest.replace("Stroll", "Speed")
+print(quest_replace)
+print(quest)
+
+# Convert String to List
+sentence = "This is a rat"
+sentence_sequence = sentence.split(" ")
+print(sentence_sequence)
+print(sentence)
+
+# Convert List to String
+join_operator = ", "
+new_sentence = join_operator.join(sentence_sequence)
+print(new_sentence)
 
 
 
